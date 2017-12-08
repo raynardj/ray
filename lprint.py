cb6eec872bb680554eb9779bd8a71619400ebefa
张晓晨(zhangxiaochen) <zhangxiaochen@zenmen.com>
2017-12-08 16:57:57 +0800

    newfiles

---
+++ lprint.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+class lprint(object):
+    def __init__(self,task="task"):
+        """
+        Standard log printing mechanism
+        l=lprint("mytask")
+        When instantiate, input task name
+        Then l.print("do_this","the log content of that")
+        """
+        self.time0=datetime.now()
+        self.timelast=datetime.now()
+        self.task=task
+        self.p("task:%s>>start"%(self.task))
+        
+    def marktime(self):
+        """
+        Mark the current time
+        """
+        return datetime.now().strftime("%Y-%m-%d_%H:%I:%S")
+    
+    def fromlast(self):
+        """
+        The timespan from last mark
+        """
+        rt=(datetime.now()-self.timelast).seconds
+        self.timelast=datetime.now()
+        return rt
+    
+    def fromstart(self):
+        """
+        The timespan from starting
+        """
+        return (datetime.now()-self.time0).seconds
+    
+    def p(self,title="log",content=""):
+        """
+        print the log line
+        title:log title, default="log"
+        content:log content, default=""
+        """
+        print("[%s]<%s|%ss,%ss>\t%s"%(title,
+                                  self.marktime(),
+                                    self.fromlast(),
+                                    self.fromstart(),
+                                  content,
+                                 ))
