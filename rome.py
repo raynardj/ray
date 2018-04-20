d2d39d641945f6420904981cfeb19bdbe7f948aa
raynardj <raynard@rasenn.com>
2018-04-20 20:40:33 +0800

    update to the latest

--- rome.py
+++ rome.py
@@ -1,21 +1,22 @@
 import os
-from multiprocessing import Pool
 
 class center:
-    def __init__(self,host_list,user_list,n_proc=6):
+    def __init__(self,host_list,user_list):
         self.host_list=host_list
         self.user_list=user_list
         self.uh_list = list([u,h] for u,h in zip(user_list,host_list))
         self.h2u = dict((h,u) for u, h in zip(user_list, host_list))
-        self.p=Pool(n_proc)
 
     def exe(self,cmd):
-        self.p.map(lambda x:os.system("ssh -l %s %s '%s'"%(x[0],x[1],cmd)),
-                   self.uh_list)
+        result=list(map(lambda x:os.system("ssh -l %s %s '%s'"%(x[0],x[1],cmd)),
+                   self.uh_list))
+        os.system(cmd)
+        list(print("[%s:result]"%(h),"\n",) for h,r in zip(self.host_list,result))
 
     def spread(self,path):
-        self.p.map(lambda x:os.system("scp -r %s %s@%s:%s"%(path,x[0],x[1],path)),
-                   self.uh_list)
+        result=list(map(lambda x:os.system("scp -r %s %s@%s:%s"%(path,x[0],x[1],path)),
+                   self.uh_list))
+        list(print("[%s:result]" % (h), "\n", ) for h, r in zip(self.host_list, result))
 
     def hexe(self,host,cmd):
         os.system("ssh -l %s %s '%s'" % (self.h2u[host],host, cmd))
