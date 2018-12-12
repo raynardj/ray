2340cc49ef8b3d602d46aabe64597881b703e8a6
raynardj <raynard@rasenn.com>
2018-12-13 00:01:16 +0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="raybox",
+    version="0.2.0",
+    author_email="raynard@rasenn.com",
+    description="Ray's tool for machine learning",
+    py_modules = ['armory.py','matchbox.py','matchbox_lego.py','lprint.py','b4tab.py']
+)
