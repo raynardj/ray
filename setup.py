c5f58dcdd3d18ac2d457503ca0d22bbb7b8fb7e5
raynardj <raynard@rasenn.com>
2018-12-13 00:23:15 +0800

    setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(
     name="raybox",
     version="0.2.0",
     author_email="raynard@rasenn.com",
-    description="Ray's tool for machine learning"
+    description="Ray's tool for machine learning",
+    py_modules=['matchbox','matchbox_lego','b4tab','armory','metrics','kmean_torch']
 )
