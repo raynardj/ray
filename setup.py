892da583511fc80a8bb1930916c445f599f74b74
raynardj <raynard@rasenn.com>
2018-12-13 12:00:03 +0800

    setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name="raybox",
+    name="ray",
     version="0.2.0",
     author_email="raynard@rasenn.com",
     description="Ray's tool for machine learning",
