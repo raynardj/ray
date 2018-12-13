de6977a08807a51fcece60396b4dda7a54b5b66f
raynardj <raynard@rasenn.com>
2018-12-13 14:26:03 +0800

    change pakcage name, add author

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(
-    name="ray",
+    name="matchbox",
     version="0.2.0",
+    author="raynardj",
     author_email="raynard@rasenn.com",
     description="Ray's tool for machine learning",
     packages=['matchbox','lprint','matchbox_lego','armory'],
