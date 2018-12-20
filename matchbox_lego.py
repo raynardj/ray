b29d6afb5945b48152b80edc6b3d9bb488a540c8
raynardj <raynard@rasenn.com>
2018-12-20 12:16:50 +0800

    deep mask attentional lstm

--- matchbox_lego.py
+++ matchbox_lego.py
@@ -90,26 +90,59 @@ class passon(nn.Module):
     def forward(self,x):
         return x
     
-# class attn_lstm(nn.Module):
-#     def __init__(self,seq_len,vocab_size,hidden_size,num_layers = 1):
-#         """
-#         attention layer for sentiment analysis
-#         seq_len: sequence length
-#         vocab_size: vocabulary size
-#         hidden_size: size for embedding, hidden state, cell state
-#         output: (batch_size, hidden_size)
-#         """
-#         super(attn_lstm,self).__init__()
-#         self.hidden_size = hidden_size
-#         self.num_layers = num_layers
-#         self.emb = nn.Embedding(vocab_size,hidden_size,)
-#         self.lstm = nn.LSTM(hidden_size,hidden_size,batch_first = True,num_layers = num_layers)
-#         self.attn = nn.Linear(hidden_size,1,bias = False)
-        
-#     def forward(self,x_input):
-#         embedded = self.emb(x_input)
-#         attn_mask = self.attn(embedded)
-#         output,(h,c) = self.lstm(embedded)
-#         output = output.permute(0,2,1)
-#         output_hidden = output.bmm(attn_mask).squeeze(-1)
-#         return output_hidden, attn_mask
+class DeepMaskAttLSTM(nn.Module):
+    def __init__(self,mask_activation="softmax",extra_mask_layers=0,**kwargs):
+        """
+        mask_activation: Activation layer to the mask, right before applying the mask
+            one of "softmax","sigmoid","passon", "passon" means no activation
+        extra_mask_layers: int number, how many hidden layers does mask_maker use   
+        Attentional LSTM
+        input_size: input dimension
+        hidden_size: hidden dimension, also the output dimention of LSTM
+        other kwargs of LSTM, most of the following is  pilferage from nn.LSTM doc:
+            input_size: mentioned above, only have to specify once
+            hidden_size: mentioned above, only have to specify once
+            num_layers: Number of recurrent layers. E.g., setting ``num_layers=2``
+                would mean stacking two LSTMs together to form a `stacked LSTM`,
+                with the second LSTM taking in outputs of the first LSTM and
+                computing the final results. Default: 1
+            bias: If ``False``, then the layer does not use bias weights `b_ih` and `b_hh`.
+                Default: ``True``
+            batch_first: If ``True``, then the input and output tensors are provided
+                as (batch, seq, feature). Default: ``False``
+            dropout: If non-zero, introduces a `Dropout` layer on the outputs of each
+                LSTM layer except the last layer, with dropout probability equal to
+                :attr:`dropout`. Default: 0
+            bidirectional: If ``True``, becomes a bidirectional LSTM. Default: ``False``
+        """
+        super(DeepMaskAttLSTM,self).__init__()
+        self.input_size = kwargs["input_size"]
+        self.hidden_size = kwargs["hidden_size"]
+        self.mask_maker_layers = []
+        self.size_in = [self.input_size]+[self.hidden_size]*extra_mask_layers
+        for i in range(extra_mask_layers):
+            self.mask_maker_layers+=[
+                nn.Linear(self.size_in[i],self.hidden_size),
+                nn.ReLU(),
+                nn.Dropout(),]
+        self.mask_maker_layers.append(nn.Linear(self.size_in[-1],1,bias=False))
+        self.mask_maker = nn.Sequential(*self.mask_maker_layers)
+        self.lstm = nn.LSTM(**kwargs)
+        if mask_activation == "softmax":
+            self.mask_act = nn.Softmax(dim = 1)
+        elif mask_activation == "sigmoid":
+            self.mask_act = nn.Sigmoid()
+        elif mask_activation == "relu":
+            self.mask_act = nn.ReLU()
+        elif mask_activation == "passon":
+            self.mask_act = passon()
+        else:
+            print("Activation type:%s not found, should be one of the following:\nsoftmax\nsigmoid\nrelu\npasson"%(mask_activation))
+
+    def forward(self,x):
+        mask_basic = self.mask_maker(x).squeeze(-1)
+        mask = self.mask_act(mask_basic).unsqueeze(1) # mask shape (bs,1,seq_leng)
+        output, (h_n,c_n) = self.lstm(x)
+        output = mask.bmm(output)
+        output = output.squeeze(1) # output shape (bs, hidden_size)
+        return output, (h_n, c_n), mask.squeeze(1)
